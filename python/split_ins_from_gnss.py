afa39243ccc90a9fca2412ce22d464261ae5ecc8
dzollo <dzollo@swift-nav.com>
2019-06-13 16:24:32 -0700

    blerg

--- python/split_ins_from_gnss.py
+++ python/split_ins_from_gnss.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python2
+#!/usr/local/bin/python3
 import sys
 import os
 import json
@@ -21,34 +21,47 @@ def main():
       if not os.path.isfile(filepath):
         print("File path {} does not exist. Exiting...".format(filepath))
         sys.exit()
+      gnss_out = ".".join(filepath.split(".")[0:-2]) + ".gps.sbp.json"
+      print("gnss output to  {}".format(gnss_out))
+      ins_out = ".".join(filepath.split(".")[0:-2])+".ins.sbp.json"
+      print("ins output to  {}".format(ins_out))
+      outfile_gnss = open(gnss_out, 'w')
+      outfile_ins = open(ins_out, 'w')
 
-      outfile_gnss = open(filepath.split(".")[0]+".gps.sbp.json",'w')
-      outfile_ins = open(filepath.split(".")[0]+".ins.sbp.json",'w')
-
-      with open(filepath) as sbp_json:
-        for json_text_line in sbp_json:
+      with open(filepath, 'r') as sbp_json:
+        for i, json_text_line in enumerate(sbp_json):
           current_msg = None
           if json_text_line.startswith('{'):
-              current_msg = json.loads(json_text_line)
-              if current_msg.get('data') != None:
-                  current_msg = current_msg.get('data')
-              msg_type = current_msg.get('msg_type')
+              try:
+                  current_msg = json.loads(json_text_line)
+                  msg_type = current_msg.get('msg_type', None)
+              except AttributeError as e:
+                  print("attribute error at line {} : {}".format(i, e))
+                  print("text was: {}".format(json_text_line))
           else:
-              print("error, no json")
-              sys.exit()
+              print("error, no json for line index {}: {}".format(i, json_text_line))
+              continue
           if (current_msg is not None):
-              if msg_type in MSGS_TO_FILTER:
-                  flags = ord(base64.b64decode(current_msg.get('payload'))[-1])
-                  if flags > 8:
-                    outfile_ins.write(json.dumps(current_msg)+'\n')
+              if msg_type is not None:
+                  if msg_type in MSGS_TO_FILTER:
+                      flags = base64.b64decode(current_msg.get('payload'))[-1]
+                      flags = base64.b64decode(current_msg.get('payload'))[-1]
+                      #print("writing to one file")
+                      if flags > 8:
+                        outfile_ins.write(json_text_line + '\n')
+                      else:
+                        outfile_gnss.write(json_text_line + '\n')
                   else:
-                    outfile_gnss.write(json.dumps(current_msg) + '\n')
+                      #print("writing to both files")
+                      outfile_ins.write(json_text_line + '\n')
+                      outfile_gnss.write(json_text_line + '\n')
               else:
-                  outfile_ins.write(json.dumps(current_msg)+'\n')
-                  outfile_gnss.write(json.dumps(current_msg) + '\n')
+                 print("error, no msg_type for line {}: {}".format(i, json_text_line))
+                 continue
           else:
-              print("error, no current_msg")
-              sys.exit()
+                print("error, json issue for line {}: {}".format(i, json_text_line))
+                continue
+
       outfile_gnss.close()
       outfile_ins.close()
 
